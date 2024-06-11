23c95bec1e1b72f0be8d32119ba4aa80c4e8ec4a
ge3224 <ge3224@gmail.com>
2024-06-10 21:06:30 -0500

    Modify Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "./src/van.ts"),
-      name: "VanTS",
-      formats: ["es", "cjs", "umd", "iife"]
+      name: "van_ts",
+      formats: ["es", "cjs", "umd", "iife"],
     },
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        ecma: 2015,
-        computed_props: false,
-        arrows: true,
-        conditionals: true,
-      },
-    }
   }
 })
