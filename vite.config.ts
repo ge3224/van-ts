196e9660d443aee7022f28d0f851d6b20bd0ac2c
ge3224 <ge3224@gmail.com>
2024-06-14 10:09:12 -0500

    Add dev dependency for build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  mode: "production",
   build: {
     lib: {
       entry: resolve(__dirname, "./src/van.ts"),
-      name: "van_ts",
+      name: "VanTS",
       formats: ["es", "cjs", "umd", "iife"],
+      fileName: (format) => `van-ts.${format}.js`,
     },
-  }
-})
+  },
+  plugins: [dts({ exclude: ["./src/main.ts"] })],
+});
